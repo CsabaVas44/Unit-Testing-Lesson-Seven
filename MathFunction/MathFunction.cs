1494722268d9e3677119e46e2a1a5b9914c3248d
Vas Csaba <vascsaba5544@gmail.com>
2022-10-14 19:50:39 +0200

    Add project files.

---
+++ MathFunction/MathFunction.cs
@@ -0,0 +1,43 @@
+﻿namespace MathFunction
+{
+    public class MathFunctions
+    {
+        public MathFunctions()
+        {
+
+        }
+        public int Area (int x, int y)
+        {
+            if (x > 0 && y > 0)
+            {
+                return x * y;
+            }
+            else return 0;
+        }
+
+        public double CircleArea(int radius)
+        {
+            if (radius > 0)
+            {
+                return Math.PI * Math.Pow(radius, 2);
+            }
+            else
+            {
+                return 0;
+            }
+            
+        }
+
+        public T Max<T>(T a, T b) where T : IComparable
+        {
+            if (a.CompareTo(b) < 0)
+            {
+                return b;
+            }
+            else
+            {
+                return a;
+            }
+        }
+    }
+}
---
+++ Tester/TesterClass.cs
@@ -0,0 +1,49 @@
+﻿using MathFunction;
+using NUnit.Framework;
+
+namespace Tester
+{
+    [TestFixture]
+    public class TesterClass
+    {
+        [Test]
+        public void AreaTest()
+        {
+            //ARRANGE
+            MathFunctions mf = new MathFunctions();
+            //ACT
+            var result = mf.Area(5, 10);
+            //ASSERT
+            Assert.That(result, Is.EqualTo(50));
+        }
+
+        [Test]
+        public void AreaTestNegative()
+        {
+            //ARRANGE
+            MathFunctions mf = new MathFunctions();
+            //ACT
+            var result = mf.Area(5, -5);
+            //ASSERT
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CirleAreaTest()
+        {
+            MathFunctions mf = new MathFunctions();
+
+            var result = mf.CircleArea(10);
+
+            Assert.That(result, Is.EqualTo(100 * Math.PI));
+        }
+        [Test]
+        public void MaxTest()
+        {
+            MathFunctions mf = new MathFunctions();
+
+            int result = mf.Max(20, 10);
+            Assert.That(result, Is.EqualTo(20));
+        }
+    }
+}
